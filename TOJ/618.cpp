78a6b76f0ae8116f946fb88bae90a70c55b702ad
Sharpless298 <sharpless298@gmail.com>
2024-02-16 09:32:16 +0800

    upd

---
+++ TOJ/618.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "Hello 8th junior programming camp practice contest!\n";
+}
