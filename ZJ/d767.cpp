f8aa6788e334faf8f8522d017f6d0655a1ebf4b0
Sharpless298 <sharpless298@gmail.com>
2024-05-03 20:05:30 +0800

    upd

--- ZJ/d767.cpp
+++ ZJ/d767.cpp
@@ -3,24 +3,19 @@
 using namespace std;
 
 int t, N, Q, lgN, ans, a, b;
-int ac[16][30005], in[30005], out[30005];
+int ac[16][30005], in[30005], out[30005], d[30005];
 vector<int> G[30005];
-
-int pow(int x, int y) {
-	int res = 1;
-	for(int i=0; i<y; i++) res*=x;
-	return res;
-}
 
 bool ancestor(int u, int v) {
 	if(in[u]<=in[v] && out[u]>=out[v]) return true;
 	return false;
 }
 
-void DFS(int u) {
+void DFS(int depth, int u) {
+	d[u] = depth;
 	in[u] = ++t;
 	for(int i:G[u]) 
-		ac[0][i] = u, DFS(i);
+		ac[0][i] = u, DFS(depth+1, i);
 	out[u] = t;
 }
 
@@ -28,8 +23,7 @@ int LCA(int u, int v) {
 	if(ancestor(u, v)) return u;
 
 	for(int i=lgN; i>=0; i--) 
-		if(!ancestor(ac[i][u], v)) u = ac[i][u], ans+=pow(2, i);
-	ans++;
+		if(!ancestor(ac[i][u], v)) u = ac[i][u];
 	return ac[0][u];
 }
 
@@ -43,16 +37,16 @@ signed main() {
 	
 	lgN = __lg(N);
 	ac[0][1] = 1;
-	DFS(1);
+	DFS(0, 1);
 	for(int i=1; i<=lgN; i++)
 		for(int j=1; j<=N; j++)
 			ac[i][j] = ac[i-1][ac[i-1][j]];
 
-	for(int i=0; i<Q; i++) {
+	for(int i=0, lca; i<Q; i++) {
 		ans = 0;
 
 		cin >> a >> b;
-		
-		cout << LCA(b, LCA(a, b)) << ' ' << ans << '\n';
+		lca = LCA(a, b);
+		cout << lca << ' ' << d[a]+d[b]-2*d[lca] << '\n';
 	}
 }
