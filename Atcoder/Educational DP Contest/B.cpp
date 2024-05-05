aa15cdf296012215f3c3c5071d2a7cfa99375353
Sharpless298 <sharpless298@gmail.com>
2024-05-05 21:49:17 +0800

    upd

--- Atcoder/Educational DP Contest/B.cpp
+++ Atcoder/Educational DP Contest/B.cpp
@@ -9,19 +9,16 @@ signed main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	memset(dp, 0x3f3f3f, sizeof(dp));
-
 	cin >> n >> k;
 	for(int i=0; i<n; i++)
 		cin >> ary[i];
 
 	dp[0] = 0;
 	for(int i=1; i<n ;i++) {
-		for(int j=1; j<=k ;j++) {
-			if(i < j) continue;
+		dp[i] = 0x3f3f3f3f;
 
+		for(int j=1; i-j>=0&&j<=k ;j++) 
 			dp[i] = min(dp[i], dp[i-j]+abs(ary[i]-ary[i-j]));
-		}
 	}
 
 	cout << dp[n-1] << '\n';
