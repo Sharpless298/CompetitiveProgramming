1485c177e013db91f9a18d69172bd07e9c6d4553
Sharpless298 <sharpless298@gmail.com>
2024-05-09 00:50:56 +0800

    mst

---
+++ TIOJ/1211.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+struct Edge {
+	int u, v, w;
+	bool operator<(Edge const &a) {
+		return w < a.w;
+	}
+};
+
+int cnt, ans;
+int parent[10005], rk[10005];
+
+Edge edges[1000005];
+
+void init(int n) {
+	cnt = ans = 0;
+	for(int i=1; i<=n; i++)
+		parent[i] = i;
+	memset(rk, 0, sizeof(int)*n);
+	memset(edges, 0, sizeof(Edge)*n);
+}
+
+int query(int x) {
+	if(parent[x] == x) return x;
+	return parent[x] = query(parent[x]);
+}
+
+void Union(int a, int b) {
+	a = query(a), b = query(b);
+	
+	if(a == b) return;
+	if(rk[a] < rk[b]) parent[a] = b;
+	else if(rk[a] > rk[b]) parent[b] = a;
+	else parent[a] = b, rk[b]++;
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+
+	while(cin>>n>>m && n) {
+		init(n);
+
+		for(int i=0; i<m; i++) 
+			cin >> edges[i].u >> edges[i].v >> edges[i].w;
+		
+		sort(edges, edges+m);
+		for(int i=0; i<m; i++) {
+			if(query(edges[i].u) != query(edges[i].v)) {
+				cnt++;
+				Union(edges[i].u, edges[i].v);
+				ans += edges[i].w;
+			}
+		}
+		
+		if(n != cnt+1) cout << -1 << '\n';
+		else cout << ans << '\n';
+	}
+}
+
