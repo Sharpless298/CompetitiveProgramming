3c01e5824d87e7e762f6d27315fa2bee509ec76f
Sharpless298 <sharpless298@gmail.com>
2024-05-16 16:21:32 +0800

    upd

--- TIOJ/1687.cpp
+++ TIOJ/1687.cpp
@@ -6,22 +6,16 @@ int N, Q, S, T, K, lgN, t, lca, d1, d2;
 int in[100005], out[100005], d[100005], ac[20][100005];
 vector<int> G[100005];
 
-int pow(int a, int b) {
-	int res = 1;
-	for(int i=0; i<b; i++) res *= a;
-	return res;
-}
-
 bool ancestor(int u, int v) {
 	if(in[u]<=in[v] && out[u]>=out[v]) return true;
 	return false;
 }
 
-void DFS(int depth, int u, int f) {
-	d[u] = depth;
+void DFS(int u, int f) {
+	d[u] = d[f]+1;
 	in[u] = ++t;
 	for(int v:G[u])
-		if(v != f) ac[0][v] = u, DFS(depth+1, v, u);
+		if(v != f) ac[0][v] = u, DFS(v, u);
 	out[u] = t;
 }
 
@@ -58,7 +52,7 @@ signed main() {
 	}
 
 	lgN = __lg(N);
-	DFS(0, 1, 0);
+	DFS(1, 0);
 	ac[0][1] = 1;
 	for(int i=1; i<=lgN; i++)
 		for(int j=1; j<=N; j++)
