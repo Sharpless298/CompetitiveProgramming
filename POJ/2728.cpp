7013fa698f393598920a5fa2d14f22875d838dbb
Sharpless298 <sharpless298@gmail.com>
2024-05-16 16:22:04 +0800

    upd;

---
+++ POJ/2728.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int x, y, z;
+};
+
+int N;
+Node node[1005];
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N;
+	for(int i=0; i<n; i++) {
+		
+	}
+}
