c50097b6129ffe4dc98ab74abd0a19309943486a
Sharpless298 <sharpless298@gmail.com>
2024-07-09 21:09:12 +0800

    upd

---
+++ ZJ/b430.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int lli;
+
+lli solve(lli a, lli b, lli m) { 
+    lli res = 0; 
+
+	a %= m, b %= m;
+	while(b) {
+        if(b&1) 
+            res = (res + a) % m;
+		a = a*2%m, b >>= 1;
+    } 
+    return res; 
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	lli a, b, n;
+
+	while(cin >> a >> b >> n) {
+		cout << solve(a, b, n) << '\n';
+	}
+}
