e46189e6f3637f560312b5ede844774040da1b96
Sharpless298 <sharpless298@gmail.com>
2024-07-22 14:49:53 +0800

    upd

--- Luogu/P1044.cpp
+++ Luogu/P1044.cpp
@@ -12,8 +12,8 @@ signed main() {
 
 	cin >> n;
 	vector<lli> f(20);
-	f[0] = f[1] = 1;
-	for(int i=2; i<=n; i++)
+	f[0] = 1;
+	for(int i=1; i<=n; i++)
 		for(int j=0; j<i; j++)
 			f[i] += f[j]*f[i-j-1];
 
