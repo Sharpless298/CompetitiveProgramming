6f4b2ea4888d71a55de63d10ef506c27ab1f8b2a
Sharpless298 <sharpless298@gmail.com>
2024-08-28 22:57:37 +0800

    upd

---
+++ AtCoder/ABC/43C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i=0; i<n; i++) 
+		cin >> a[i];	
+
+	sort(a.begin(), a.end());
+	int l = *a.begin(), r = *a.rbegin();
+	
+	int ans = INT_MAX;
+	for(int i=l; i<=r; i++) {
+		int res = 0;
+		
+		for(int j=0; j<n; j++)
+			res += (a[j]-i)*(a[j]-i);
+		
+		ans = min(ans, res);
+	}
+
+	cout << ans << '\n';
+}
+
