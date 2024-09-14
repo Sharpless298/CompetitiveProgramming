7aff95645e29e877f79c6fc6396d730994dc012b
Sharpless298 <sharpless298@gmail.com>
2024-09-15 02:41:03 +0800

    upd

--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+vector<Node> seg;
+
+void pull(){}
+
+void build(int L = 0, int R = n, int id = 0) {
+	if(R-L == 1) {
+		seg[id] = a[L];
+		return;
+	}
+
+	int M = (L+R)/2;
+	build(L, M, id*2+1);
+	build(M, R, id*2+2);
+	pull(id);
+}
+
+void update(int l, int r, int L = 0, int R = n, int id = 0) {
+	if(l>=R || r<=L) return;
+	if(l<=L && R<=r) return seg[id];
+
+	int M = (L+R)/2;
+	update(l, r, L, M, id*2+1);
+	update(l, r, M, R, id*2+2);
+	pull(id);
+}
 
 signed main() {
 	ios_base::sync_with_stdio(false);
