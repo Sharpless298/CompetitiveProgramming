ff0e43fcb7463d15495762c736bc11a7671a6bb5
Sharpless298 <sharpless298@gmail.com>
2024-10-12 06:57:30 +0800

    upd

--- Library Checker/static_convex_hull.cpp
+++ Library Checker/static_convex_hull.cpp
@@ -5,8 +5,6 @@
 #include <cassert>
 using namespace std;
 
-// const double EPS = 1e-8;
-
 template<typename T>
 pair<T, T> operator+(pair<T, T> a, pair<T, T> b) { return make_pair(a.first+b.first, a.second+b.second); }
 template<typename T>
@@ -29,20 +27,8 @@ T abs(pair<T, T> a) { return sqrt(dot(a, a)); }
 template<typename T>
 int ori(pair<T, T> &a, pair<T, T> &b, pair<T, T> &p) {
 	T res = cross(a-p, b-p);
-	// if(fabs(res) < EPS) return 0;
 	if(res == 0) return 0;
 	return res>0 ? 1 : -1;
-}
-
-template<typename T>
-bool collinear(pair<T, T> &a, pair<T, T> &b, pair<T, T> &p) {
-	// return fabs(cross(a-p, b-p)) < EPS;
-	return cross(a-p, b-p) == 0;
-}
-
-template<typename T>
-bool onSegment(pair<T, T> &a, pair<T, T> &b, pair<T, T> &p) {
-	return collinear(a, b, p) && dot(a-p, b-p)<0;
 }
 
 template<typename T>
@@ -53,33 +39,10 @@ bool intersect(pair<T, T> &a, pair<T, T> &b, pair<T, T> &c, pair<T, T> &d) {
 }
 
 template<typename T>
-pair<T, T> intersection(pair<T, T> &a, pair<T, T> &b, pair<T, T> &c, pair<T, T> &d) {
-	assert(intersect(a, b, c, d));
-	return a + (b-a) * cross(a-c, d-c) / cross(d-c, b-a);
-}
-
-template<typename T>
-int quadrant(pair<T, T> &a) {
-	if(a.first>0 && a.second>=0) return 1;
-	if(a.first<=0 && a.second>0) return 2;
-	if(a.first<0 && a.second<=0) return 3;
-	if(a.first>=0 && a.second<0) return 4;
-	return -1;
-}
-
-template<typename T>
-bool cmp(pair<T, T> &a, pair<T, T> &b) {
-	if(quadrant(a) != quadrant(b))
-		return quadrant(a) < quadrant(b);
-	if(cross(a, b) == 0) return abs2(a) < abs2(b);
-	return cross(a, b) > 0;
-}
-
-template<typename T>
 vector<pair<T, T>> getConvexHull(vector<pair<T, T>> &pnts) {
 	sort(pnts.begin(), pnts.end());
-	// pnts.resize(unique(pnts.begin(), pnts.end()-pnts.begin()));
-	// if(pnts < 3) return pnts;
+	pnts.resize(unique(pnts.begin(), pnts.end())-pnts.begin());
+	if(pnts.size() < 3) return pnts;
 
 	vector<pair<T, T>> hull;
 	for(int i=0; i<2; i++) {
@@ -96,3 +59,27 @@ vector<pair<T, T>> getConvexHull(vector<pair<T, T>> &pnts) {
 	return hull;
 }
 
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int T;
+	cin >> T;
+	while(T--) {
+		int n;
+		cin >> n;
+
+		if(n == 0) {
+			cout << 0 << '\n';
+			continue;
+		}
+
+		vector<pair<long long, long long>> v(n);
+		for(int i=0; i<n; i++) 
+			cin >> v[i].first >> v[i].second;
+
+		v = getConvexHull(v);
+		cout << v.size() << '\n';
+		for(auto &i:v) cout << i.first << ' ' << i.second << '\n';
+	}
+}
