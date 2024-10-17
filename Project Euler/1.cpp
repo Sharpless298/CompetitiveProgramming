476620b0c43441419ae9bc3f2ad52e2624486145
Sharpless298 <sharpless298@gmail.com>
2024-10-18 07:54:14 +0800

    upd

--- Project Euler/1.cpp
+++ Project Euler/1.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 signed main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr), cout.tie(nullptr);
-
 	int ans = 0;
 	for(int i=1; i<1000; i++) 
 		if(i%3==0 || i%5==0) ans += i;
