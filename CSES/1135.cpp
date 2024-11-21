308b54bc9032b5188bb8313902d20a273c5c26a4
Sharpless298 <sharpless298@gmail.com>
2024-11-22 07:19:21 +0800

    upd

--- CSES/1135.cpp
+++ CSES/1135.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int lgN, t;
-vector<int> in, out;
+vector<int> in, out, d;
 vector<vector<int>> G, ac;
 
 void DFS(int u, int f) {
 	in[u] = t++;
-	for(int &v:G[u]) {
+	for(int v : G[u]) {
 		if(v == f) continue;
 
+		d[v] = d[u] + 1;
 		ac[v][0] = u;
-		for(int i=1; i<=lgN; i++) 
+		for(int i=1; i<=lgN; i++)
 			ac[v][i] = ac[ac[v][i-1]][i-1];
 		DFS(v, u);
 	}
@@ -26,7 +27,7 @@ bool ancestor(int u, int v) {
 int LCA(int u, int v) {
 	if(ancestor(u, v)) return u;
 
-	for(int i=lgN; i>=0; i--)
+	for(int i=lgN; i>=0; i--) 
 		if(!ancestor(ac[u][i], v)) u = ac[u][i];
 
 	return ac[u][0];
@@ -35,32 +36,29 @@ int LCA(int u, int v) {
 signed main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
+
 	int n, q;
 	cin >> n >> q;
 	G.resize(n);
-	for(int i=1; i<n; i++) {
-		int u;
-		cin >> u;
-		u--;
-		G[i].push_back(u);
-		G[u].push_back(i);
+	for(int i=0; i<n-1; i++) {
+		int u, v;
+		cin >> u >> v;
+		u--, v--;
+		G[u].push_back(v);
+		G[v].push_back(u);
 	}
-
+	
 	lgN = __lg(n);
 	in.resize(n);
 	out.resize(n);
 	ac.resize(n, vector<int>(lgN+1));
 	ac[0][0] = 0;
+	d.resize(n);
 	DFS(0, -1);
-	/*
-	for(int i=1; i<=lgN; i++)
-		for(int j=1; j<=N; j++)
-			ac[j][i] = ac[ac[j][i-1]][i-1];*/
-
 	while(q--) {
 		int a, b;
 		cin >> a >> b;
-		cout << LCA(a-1, b-1)+1 << '\n';
+		a--, b--;
+		cout << d[a]+d[b]-2*d[LCA(a, b)] << '\n';
 	}
 }
--- templates/gcd.cpp
+++ templates/gcd.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-long long int gcd(int a, int b) {
+long long gcd(long long a, long long b) {
 	return b ? gcd(b, a%b) : a;
 }
 
-long long int gcd(long long int a, long long int b) {
+long long gcd(long long a, long long b) {
 	while(b){
         a %= b;
         a ^= b, b ^= a, a ^= b;
