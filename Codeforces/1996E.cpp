667bbcac0e51f537f1511884d42136f568a50311
Sharpless298 <sharpless298@gmail.com>
2025-01-15 22:28:44 +0800

    upd

--- Codeforces/1996E.cpp
+++ Codeforces/1996E.cpp
@@ -1,9 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef long long int lli;
 
 const int MOD = 1e9+7;
 
@@ -16,20 +12,21 @@ signed main() {
 	while(T--) {
 		string s;
 		cin >> s;
-
-		int n = s.size();
+		int n = (int)s.size();
 		vector<int> a(n);
 		for(int i=0; i<n; i++)
-			a[i] = (s[i]=='1' ? 1 : -1);
+			a[i] = (s[i]=='0' ? -1 : 1);
 
 		vector<int> pref(n+1);
 		for(int i=0; i<n; i++)
 			pref[i+1] = pref[i] + a[i];
 
-		lli ans = 0;
-		map<int, lli> mp;
-		for(int i=1; i<=n; i++) {
-			ans = (ans + 
+		long long ans = 0;
+		map<int, long long> mp;
+		for(int i=0; i<=n; i++) {
+			ans = (ans + (n-i+1) * mp[pref[i]]) % MOD;
+			mp[pref[i]] = (mp[pref[i]] + i+1) % MOD;
 		}
+		cout << ans << '\n';
 	}
 }
