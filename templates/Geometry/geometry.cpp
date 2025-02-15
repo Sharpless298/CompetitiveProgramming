e8999b82a72f6398d1d99df44394598a082f1a9a
Sharpless298 <sharpless298@gmail.com>
2025-02-16 07:01:02 +0800

    upd

--- templates/Geometry/geometry.cpp
+++ templates/Geometry/geometry.cpp
@@ -6,8 +6,10 @@ struct Point {
 	Point(int _x, int _y) : x(_x), y(_y) {}
 	Point operator +(Point p) { return Point(x+p.x, y+p.y); }
 	Point operator -(Point p) { return Point(x-p.x, y-p.y); }
-	Point operator *(int c) { return Point(x*c, y*c); }
-	Point operator /(int c) { return Point(x/c, y/c); }
-	T dot(Point p) { return x*p.x + y*p.y; }
-	T cross(Point p) { return x*p.y - y*p.x; }
+	Point operator *(T c) { return Point(x*c, y*c); }
+	Point operator /(T c) { return Point(x/c, y/c); }
 };
+template <typename T>
+T dot(Point<T> a, Point<T> b) { return a.x*b.x + a.y*b.y; }
+template <typename T>
+T cross(Point<T> a, Point<T> b) { return a.x*b.y - a.y*b.x; }
