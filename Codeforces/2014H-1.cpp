3f33e3b39d9fa8f3f24c6e26dfc4d3a7b98f50e8
Sharpless298 <sharpless298@gmail.com>
2025-02-28 20:06:56 +0800

    upd

--- Codeforces/2014H-1.cpp
+++ Codeforces/2014H-1.cpp
@@ -22,9 +22,9 @@ signed main() {
 		for(int i=0; i<q; i++) 
 			cin >> qry[i].l >> qry[i].r, qry[i].id = i;
 
-		const int K = (int)sqrt(n)+1;
+		const int K = (int)sqrt(n);
 		auto cmp = [&](Query &aa, Query &bb) {
-			return aa.l/K==bb.l/K ? ((aa.l/K)&1 ? aa.r<bb.r : aa.r>bb.r) : (aa.l<bb.l);
+			return (aa.l/K)^(bb.l/K) ? aa.l < bb.l : ((aa.l/K)&1 ? aa.r<bb.r : aa.r>bb.r);
 		};
 		sort(qry.begin(), qry.end(), cmp);
 
