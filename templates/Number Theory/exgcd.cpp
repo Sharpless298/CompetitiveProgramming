108b7b319e21ac3117421b96b679cd75ee63bf11
Sharpless298 <sharpless298@gmail.com>
2025-05-25 03:48:20 +0800

    upd

--- templates/Number Theory/exgcd.cpp
+++ templates/Number Theory/exgcd.cpp
@@ -1,5 +1,7 @@
 pair<int, int> exgcd(int a, int b) {
-	if (b == 0) return make_pair(1, 0);
+	if (b == 0) {
+		return make_pair(1, 0);
+	}
 	pair<int, int> res = exgcd(b, a % b);
 	return make_pair(res.second, res.first - a / b * res.second);
 }
